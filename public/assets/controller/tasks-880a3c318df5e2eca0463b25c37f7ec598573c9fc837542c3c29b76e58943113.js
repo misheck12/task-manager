a5ba0550818ad1dc36cf89e4c3aafe83cee7286a
Mauro Baptista <mauro.baptista@carnou.com>
2016-11-28 16:21:42 -0200

    Allow heroku auto assets precompilation

--- public/assets/controller/tasks-880a3c318df5e2eca0463b25c37f7ec598573c9fc837542c3c29b76e58943113.js
+++
@@ -1,8 +0,0 @@
-$(function () {
-  //Datetimepicker plugin
-  $('.datetimepicker').bootstrapMaterialDatePicker({
-      format: 'dddd DD MMMM YYYY - HH:mm',
-      clearButton: true,
-      weekStart: 1
-  });
-});
